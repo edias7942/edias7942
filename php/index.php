c267eb13dbe15c67e754db14d1e00347ba7ea1e0
Emerson Dias <edias7942@gmail.com>
2024-02-15 20:50:24 +0000

    adicionando php

---
+++ php/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    //PHP = Personal Home Page
+
+    //Variáveis
+    //Variável da variável
+    //Concatenar
+    //Comparar valores
+    //<= <= == !=
+    //=== !==
+    //Looping
+    //Funções
+    //Classes
+
+    // $nome = 'Emerson';
+    // $Emerson = 'Bla bla';
+
+    // echo 'O meu nome é '.$$nome;
+
+    $idade = '20';
+
+    if($idade >= 20) {
+        echo 'Verdadeiro';
+    }else{
+        echo 'Falso';
+    }
+?>
+</body>
+</html>
